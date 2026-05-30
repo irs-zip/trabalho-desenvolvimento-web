eeeb58ff331d688f3532f5767c03430bf3ea2c5e
philip <49840105+philipemarcel@users.noreply.github.com>
2026-05-30 14:33:43 -0300

    mudanças na validação

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 //função para validar envio de formulario usando as classes do bootstrap
 const formulario = document.getElementById('formulario');
+formulario.addEventListener('input', function (evento) {
+    if (formulario.senha.value !== formulario.confirmarSenha.value) {
+        evento.preventDefault();
+        evento.stopPropagation();
+        formulario.confirmarSenha.setCustomValidity('As senhas não coincidem');
+        if (formulario.confirmarSenha.value === '') {
+            document.getElementById('confirm').innerHTML = 'Por favor, confirme sua senha.';
+        } else{
+            document.getElementById('confirm').innerHTML = 'As senhas não coincidem.';
+        }
+    } else {
+        formulario.confirmarSenha.setCustomValidity('');
+    }
+});
 formulario.addEventListener('submit', function (evento) {
     if (!formulario.checkValidity()) {
         evento.preventDefault();
         evento.stopPropagation();
     }
-    if (formulario.senha.value !== formulario.confirmarSenha.value) {
-        evento.preventDefault();
-        evento.stopPropagation();
-        formulario.confirmarSenha.setCustomValidity('As senhas não coincidem');
-    } {
-        formulario.classList.add('was-validated');
-    }
+
+    formulario.classList.add('was-validated');
 });
 
